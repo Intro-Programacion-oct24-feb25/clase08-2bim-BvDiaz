ff8f3c4278720c3709848641120c31fb680c2f0c
Bryan Diaz <vicentepomas.2007@gmail.com>
2025-01-29 11:51:32 -0500

    ARRAY TO LIST

--- Ejercicios07/src/lectura01/PruebaLeerArchivoTexto.java
+++ Ejercicios07/src/lectura01/PruebaLeerArchivoTexto.java
@@ -5,8 +5,8 @@ public class PruebaLeerArchivoTexto {
 
     public static void main(String args[]) {
         // LeerArchivoTexto.leerRegistros01();
-        // LeerArchivoTexto.leerRegistros02();
-        LeerArchivoTexto.leerRegistros03();
+         LeerArchivoTexto.leerRegistros02();
+        //LeerArchivoTexto.leerRegistros03();
         // LeerArchivoTexto.leerRegistros04();
 
     } // fin de main
